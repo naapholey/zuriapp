36aad5679bb03032a46b7bae5936e740b0068837
naapholei <annettetettey@gmail.com>
2026-06-26 10:01:49 +0000

    minor

--- terraform/compute_k3s.tf
+++ terraform/compute_k3s.tf
@@ -223,7 +223,6 @@ resource "aws_iam_role_policy" "vpc_flow_log_policy" {
   role = aws_iam_role.vpc_flow_log_role.id
 
   policy = jsonencode({
-    Version = "2012-10-17"
     Statement = [
       {
         Sid    = "VPCFlowLogsToCloudWatch"
